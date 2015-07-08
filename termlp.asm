de58d571145dfe2191d0578028d25c3e96d78dee
finnapancake13 <zachrox68@aol.com>
2015-07-08 19:48:06 -0400

    Update termlp.asm

--- termlp.asm
+++ termlp.asm
@@ -15,18 +15,44 @@
 
 ostloop:
     ; ask kernel for input, info get's pushed to stack
-    pop eax
+    pop eax ; Load first argument
     cmp eax, cvers
     je version
+    cmp eax, chelp
+    je help
+    cmp eax, cexit
+    je exit
+    
+    move ebx, unkno
+    jmp print
     
     version:
-        ; tell kernel to print vers
+        mov ebx, vers
+        jmp print
+        ret
+    help:
+        move ebx, help
+        jmp print
+        ret
+    exit:
+        move ebx, emsg
+        jmp print
+        jmp endloop
+        
+    print:
+        ; print data from ebx using kernel
         ret
     
     jmp ostloop
+endloop:
     
     
 ;8086 define data bullshit here
+    unkno:  db 'Unknown command. Execute "commands" to see a list of usable commands'
+    cexit:  db 'stop'
     cvers:  db 'version'
+    chelp:  db 'help'
+    emsg:   db 'Exiting OST'
     vers:   db 'OST Version 1.00 is running'
+    help:   db 'OST, or Open Server Terminal is a text-based open source operating system written in assembly code for developers and servers by an online github users. Visit the github of OST to report bugs or contact the developer. Type "command" to get a list of usable commands.'
     
