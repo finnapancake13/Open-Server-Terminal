578e0f01ec042f39c3f82086cdb66b632894ccae
finnapancake13 <zachrox68@aol.com>
2015-07-08 18:48:29 -0400

    termlp.asm
    terminal loop

---
+++ termlp.asm
@@ -0,0 +1,18 @@
+; Open Server Terminal ver 1.0
+; use this source in your programs or alterration distros. using a direct copy and distributing is not allowed.
+
+; the following messages / checkups will be shown and performed at boot:
+
+; OST ver <version>
+;
+; Successful Boot  [OK / NO]
+; Bootfiles Safe   [OK / NO]
+; User Cfg Setup   [OK / NO]
+; Enter username:
+; Enter password:
+; <if user cfg setup> Successfully logged in to <username>
+; <if user cfg not setup> Successfully created account <username>
+; once logged in start loop:
+
+ostloop:
+    
